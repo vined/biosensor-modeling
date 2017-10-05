e77dfaa5472bea8af96a10a7fa107817d1ec3be3
Edvinas Naraveckas <e.naraveckas@gmail.com>
2017-10-05 22:39:26 +0300

    Some comments and todo

--- src/main.cc
+++ src/main.cc
@@ -19,5 +19,26 @@ int main() {
     for (double val : net)
         cout << val << endl;
 
+/*
+Create values net w_t, when k(0..M)
+
+
+    - Modeling -
+i - electric current
+n_e - electrons count (1 or 2)
+F - Faraday's constant 96485.3365 C/mol
+D_Pe - difussion coeficient in ferment near electrode
+
+main formula from 5.4.2:
+i(t_k) = n_e * F * D_Pe * ( - p0 + p1 - p2 )
+
+p0 = ( (x[1]-x[2]) / (x[1]*x[2]) ) * P[k][0]
+p2 = ( x[2] / (x[1] * (x[2]-x[1])) ) * P[k][1]
+p = ( x[1] / (x[2] * (x[2]-x[1])) ) * P[k][2]
+
+
+Next - implement 5.6.4 and 5.6.5
+
+*/
     return 0;
 }
