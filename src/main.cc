f8cf47f2aa1fa6b4fc1008883fee7d16f7e331df
Edvinas Naraveckas <e.naraveckas@gmail.com>
2017-12-22 17:50:49 +0200

    Moved reading to manager process

--- src/main.cc
+++ src/main.cc
@@ -14,7 +14,6 @@
 
 
 #define ALL_ARG_COUNT 1+5+9
-#define NODES 3
 
 #define OPEN_MPI_MANAGER_ID 0
 
@@ -49,23 +48,19 @@ int main(int argc, char *argv[]) {
     std::vector<double> D_s;
     std::vector<double> D_p;
 
-//    double c_size = 0;
-//    double *c1;
-//    double *c2;
-
-    double C1[NODES] = {0.02, 0.025, 0.03};
-    double C2[NODES] = {0.005, 0.01, 0.019};
+    int c_size = 3;
+    double C1[c_size] = {0.02, 0.025, 0.03};
+    double C2[c_size] = {0.005, 0.005, 0.005};
 
 
     // Calculation results
 
-    double I[NODES];
-    double T[NODES];
+    double I[c_size];
+    double T[c_size];
 
 
     // Retrieving parameters
 
-    std::cout << processor_id << " Arguments count" << argc << std::endl;
     if (argc == 3) {
         grid_params = getDemoGridParameters();
         model_params = getDemoModelParameters();
@@ -103,69 +98,34 @@ int main(int argc, char *argv[]) {
 
     // Read C1
 
-    MPI_File c1_data;
-    int ierr1 = MPI_File_open(MPI_COMM_WORLD, argv[1], MPI_MODE_RDONLY, MPI_INFO_NULL, &c1_data);
-    if (ierr1) {
-        std::cout << "Couldn't open file " << argv[1] << endl;
-        MPI_Finalize();
-        return 1;
+    if (processor_id == OPEN_MPI_MANAGER_ID) {
+        MPI_File c1_data;
+        int ierr1 = MPI_File_open(MPI_COMM_WORLD, argv[1], MPI_MODE_RDONLY, MPI_INFO_NULL, &c1_data);
+//        if (ierr1) {
+//            std::cout << "Couldn't open file " << argv[1] << endl;
+//            MPI_Finalize();
+//            return 1;
+//        }
+
+//        MPI_Offset offset;
+//        MPI_File_get_size(c1_data, &offset);
+//        int fileSize = offset / sizeof(char);
+//        char *buf1 = (char *) malloc(fileSize);
+//
+//        MPI_File_read(c1_data, buf1, fileSize, MPI_CHAR, MPI_STATUS_IGNORE);
+//
+//        int c1_size = getLinesCount(buf1, fileSize);
+//        std::cout << "size " << c1_size << std::endl;
+//        double *c1 = (double *) malloc(c1_size);
+//        readDoubles(buf1, fileSize, c1);
+//        free(c1);
+//
+//        free(buf1);
+//        MPI_File_close(&c1_data);
     }
 
-    MPI_Offset offset;
-    MPI_File_get_size(c1_data, &offset);
-    int fileSize = offset / sizeof(char);
-    char *buf1 = (char *) malloc(fileSize);
-
-    MPI_File_read(c1_data, buf1, fileSize, MPI_CHAR, MPI_STATUS_IGNORE);
-
-//    c_size = getLinesCount(buf1, fileSize);
-//    std::cout << "size " << c_size << std::endl;
-//    double *c1 = (double *) malloc(c_size);
-//    c1 = (double *) malloc(c_size);
-//    if (processor_id == OPEN_MPI_MANAGER_ID) {
-//        readDoubles(buf1, fileSize, c1);
-//    }
-//    free(c1);
-    free(buf1);
 
     MPI_Barrier(MPI_COMM_WORLD);
-    MPI_File_close(&c1_data);
-    MPI_Barrier(MPI_COMM_WORLD);
-
-    // Read C2
-
-//    MPI_File c2_data;
-//    std::cout << "Reading file: " << argv[2] << std::endl;
-//    int ierr2 = MPI_File_open(MPI_COMM_WORLD, argv[2], MPI_MODE_RDONLY, MPI_INFO_NULL, &c2_data);
-//    if (ierr2) {
-//        std::cout << "Couldn't open file " << argv[2] << endl;
-//        MPI_Finalize();
-//        return 1;
-//    }
-
-//        MPI_File_get_size(c2_data, &offset);
-//        fileSize = offset / sizeof(char);
-//        char *buf2 = (char *) malloc(fileSize);
-//
-//        MPI_File_read(c2_data, buf2, fileSize, MPI_CHAR, MPI_STATUS_IGNORE);
-//
-//        c2 = (double *) malloc(c_size);
-//        readDoubles(buf2, fileSize, c2);
-//        free(buf2);
-
-//        for (unsigned i = 0; i < c_size; i++) {
-//            std::cout << "C1 " << c1[i] << std::endl;
-//        }
-//        for (unsigned i = 0; i < c_size; i++) {
-//            std::cout << "C1 " << c1[i] << "\tC2 " << c2[i] << std::endl;
-//        }
-
-    // Split the load
-//    }
-
-//    MPI_Barrier(MPI_COMM_WORLD);
-//    MPI_File_close(&c2_data);
-//    MPI_Barrier(MPI_COMM_WORLD);
 
 
     // Approximation
@@ -192,7 +152,7 @@ int main(int argc, char *argv[]) {
 
     std::cout << processor_id << " I approximation finished in " << difftime(time(NULL), t1) << "s" << std::endl;
 
-//    MPI_Barrier(MPI_COMM_WORLD);
+    MPI_Barrier(MPI_COMM_WORLD);
 
     MPI_Gather(
             &I_t.first, 1, MPI_DOUBLE,
@@ -206,15 +166,12 @@ int main(int argc, char *argv[]) {
             OPEN_MPI_MANAGER_ID, MPI_COMM_WORLD
     );
 
-//    MPI_Barrier(MPI_COMM_WORLD);
-//    free(c1);
-
     MPI_Barrier(MPI_COMM_WORLD);
 
     // Results
 
     if (processor_id == OPEN_MPI_MANAGER_ID) {
-        for (unsigned i; i < NODES; i++) {
+        for (unsigned i; i < processors_count; i++) {
             std::cout << i << " I = " << I[i] << " A m^(-2), t* = " << T[i] << " s" << std::endl;
         }
     }
